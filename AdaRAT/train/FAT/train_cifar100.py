595bce4fe38186fcd5550e45867389059901a9af
xiyan0524 <85203278+xiyan0524@users.noreply.github.com>
2024-05-28 12:01:45 +0800

    Update train_cifar100.py

--- AdaRAT/train/FAT/train_cifar100.py
+++ AdaRAT/train/FAT/train_cifar100.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
     method = "FAT"
     if args.FixRAT:
         method += "_FixRAT"
-    elif args.AdaRAT:
-        method += "_AdaRAT"
+        if args.AdaRAT:
+            method += "_AdaRAT"
     print("start training!")
     print("train method: %s" % method)
 
